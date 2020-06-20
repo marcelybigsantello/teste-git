21e0d014b2b4cc2ce2e596fe740af8e6f503cae6
Marcely Santello <marcely@sbsoftware.com.br>
2020-06-19 22:05:21 -0300

    Lista criada e instanciada com alguns dos estados brasileiros, laço foreach para printar todos os elementos da lista

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Projeto_teste
 {
@@ -10,9 +11,33 @@ namespace Projeto_teste
 
             Console.WriteLine("Tenha esperança, vai passar!");
             Console.WriteLine("Eu quero sair");
-            Console.WriteLine("Eu quero viajar");
-            Console.WriteLine("Eu quero conhecer o mundo!");
             Console.ReadLine();
+
+            //Criação e instanciação de lista
+            List<string> estados = new List<string>();
+            estados.Add("Amazonas");
+            estados.Insert(0, "Acre");
+            estados.Insert(2, "Amapá");
+            estados.Insert(1, "Alagoas");
+            estados.Add("Bahia");
+            estados.Add("Ceará");
+            estados.Add("Espírito Santo");
+            estados.Add("Goiás");
+            estados.Add("Mato Grosso do Sul");
+            estados.Add("Minas Gerais");
+            estados.Add("Paraná");
+            estados.Add("Rio de Janeiro");
+            estados.Add("Rio Grande do Norte");
+            estados.Add("Santa Catarina");
+            estados.Add("São Paulo");
+
+            foreach(string obj in estados)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("\nQuantidade de estados na lista: "+estados.Count);
+
         }
     }
 }
